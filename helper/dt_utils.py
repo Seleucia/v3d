8014c14dcdc604bab00759c52c534dc5d6d5e24f
Seleucia <coskunh1986@gmail.com>
2016-07-01 02:19:15 +0300

    cnn data loading.

--- helper/dt_utils.py
+++ helper/dt_utils.py
@@ -136,6 +136,8 @@ def multi_thr_read_full_joints_cnn(base_file,max_count,p_count,sindex,istest,get
             tmp_folder=joints_file+actor+"/"+sq+"/"
             tmp_folder_img=img_folder+actor+"/"+sq.replace('.cdf','')+"/"
             id_list=os.listdir(tmp_folder)
+            if os.path.exists(tmp_folder_img)==False:
+                continue
             img_count=len(os.listdir(tmp_folder_img))
             min_count=img_count
             if(len(id_list)<img_count):
