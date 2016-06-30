b449d9848398b0e69a27d57929a7aa5087cb555c
Seleucia <coskunh1986@gmail.com>
2016-06-30 18:39:35 +0300

    cnn data loading.

--- helper/dt_utils.py
+++ helper/dt_utils.py
@@ -143,7 +143,7 @@ def multi_thr_read_full_joints_cnn(base_file,max_count,p_count,sindex,istest,get
                 min_count=len(id_list)
             id_list=id_list[0:min_count]
             joint_list=[tmp_folder + p1 for p1 in id_list]
-            midlayer_list=[actor+'/'+sq.replace('cdf','mp4')+'/'+p1 for p1 in id_list]
+            midlayer_list=[actor+'/'+sq.replace('cdf','mp4')+'/'+p1.replace('.txt','.png') for p1 in id_list]
             pool = ThreadPool(1000)
             results = pool.map(load_file, joint_list)
             pool.close()
