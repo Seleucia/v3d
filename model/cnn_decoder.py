2fb163cf9262f85d95d596228b6fe56302cbf128
Seleucia <coskunh1986@gmail.com>
2016-07-04 02:39:00 +0300

    cnn data loading.

--- model/cnn_decoder.py
+++ model/cnn_decoder.py
@@ -61,7 +61,7 @@ class cnn_decoder(object):
 
         h2=HiddenLayer(rng,h1.output,1024,2048,activation=do_nothing) #previus regression layer
 
-        h3=HiddenLayer(rng,h1.output,2048,1024,activation=do_nothing) #previus regression layer
+        h3=HiddenLayer(rng,h2.output,2048,1024,activation=do_nothing) #previus regression layer
 
         lreg=LogisticRegression(rng,h3.output,1024,params['n_output'])
         self.output = lreg.y_pred
