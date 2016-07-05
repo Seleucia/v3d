e8d178f9178490c67fe99ae2ad4561448c25734d
Seleucia <coskunh1986@gmail.com>
2016-07-05 12:35:35 +0300

    cnn data loading.

--- helper/config.py
+++ helper/config.py
@@ -33,7 +33,7 @@ def get_params():
 
    # learning parameters
    params['momentum']=0.9    # the params for momentum
-   params['lr']=0.0001
+   params['lr']=0.001
    params['learning_rate_decay']= 0.998
    params['squared_filter_length_limit']=15.0
    params['n_epochs']=25600
