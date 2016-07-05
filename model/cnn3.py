b941ed3143f5bdeb7eeeb88c75d9454354bf71e7
Seleucia <coskunh1986@gmail.com>
2016-07-05 12:35:19 +0300

    cnn data loading.

--- model/cnn3.py
+++ model/cnn3.py
@@ -10,62 +10,63 @@ from helper.optimizer import RMSprop
 # theano.config.exception_verbosity="high"
 dtype = T.config.floatX
 
-class cnn3(object):
+class cnn(object):
     def __init__(self,rng,params,cost_function='mse',optimizer = RMSprop):
 
         lr=params["lr"]
         batch_size=params["batch_size"]
-        sequence_length=params["seq_length"]
 
         # minibatch)
-        X = T.matrix(name="input",dtype=dtype) # batch of sequence of vector
+        X = T.tensor4(name="input",dtype=dtype) # batch of sequence of vector
         Y = T.matrix(name="output",dtype=dtype) # batch of sequence of vector
         is_train = T.iscalar('is_train') # pseudo boolean for switching between training and prediction
 
         #CNN global parameters.
         subsample=(1,1)
         p_1=0.5
-        border_mode="same"
+        border_mode="full"
         cnn_batch_size=batch_size
         pool_size=(2,2)
 
         #Layer1: conv2+pool+drop
-        filter_shape=(128,1,10,10)
-        input_shape=(cnn_batch_size,1,144,176) #input_shape= (samples, channels, rows, cols)
-        input= X.reshape(input_shape)
+        filter_shape=(36,3,9,9)
+        input_shape=(cnn_batch_size,3,112,112) #input_shape= (samples, channels, rows, cols)
+        input= X.dimshuffle(0,3,1,2)
         c1=ConvLayer(rng, input,filter_shape, input_shape,border_mode,subsample, activation=nn.relu)
         p1=PoolLayer(c1.output,pool_size=pool_size,input_shape=c1.output_shape)
-        dl1=DropoutLayer(rng,input=p1.output,prob=p_1,is_train=is_train)
+
 
         #Layer2: conv2+pool
         subsample=(1,1)
-        filter_shape=(256,p1.output_shape[1],3,3)
-        c2=ConvLayer(rng, dl1.output, filter_shape,p1.output_shape,border_mode,subsample, activation=nn.relu)
+        filter_shape=(72,p1.output_shape[1],3,3)
+        c2=ConvLayer(rng, p1.output, filter_shape,p1.output_shape,border_mode,subsample, activation=nn.relu)
         p2=PoolLayer(c2.output,pool_size=pool_size,input_shape=c2.output_shape)
 
         #Layer3: conv2+pool
-        filter_shape=(256,p2.output_shape[1],3,3)
+        filter_shape=(72,p2.output_shape[1],3,3)
         c3=ConvLayer(rng, p2.output,filter_shape,p2.output_shape,border_mode,subsample, activation=nn.relu)
         p3=PoolLayer(c3.output,pool_size=pool_size,input_shape=c3.output_shape)
 
+        #Layer5: hidden
+        n_in= reduce(lambda x, y: x*y, p3.output_shape[1:])
+        x_flat = p3.output.flatten(2)
 
-        #Layer3: conv2+pool
-        filter_shape=(128,p3.output_shape[1],3,3)
-        c4=ConvLayer(rng, p3.output,filter_shape,p3.output_shape,border_mode,subsample, activation=nn.relu)
-        p4=PoolLayer(c4.output,pool_size=pool_size,input_shape=c4.output_shape)
+        h1=HiddenLayer(rng,x_flat,n_in,512,activation=nn.relu)
+        dl1=DropoutLayer(rng,input=h1.output,prob=p_1,is_train=is_train)
 
-        #Layer4: hidden
-        n_in= reduce(lambda x, y: x*y, p4.output_shape[1:])
-        x_flat = p4.output.flatten(2)
+        h2=HiddenLayer(rng,dl1.output,n_in,2048,activation=nn.relu)
+        dl2=DropoutLayer(rng,input=h2.output,prob=p_1,is_train=is_train)
 
-        h1=HiddenLayer(rng,x_flat,n_in,1024,activation=nn.relu)
+        h3=HiddenLayer(rng,dl2.output,n_in,4096,activation=nn.relu)
+        dl3=DropoutLayer(rng,input=h3.output,prob=p_1,is_train=is_train)
 
+        h4=HiddenLayer(rng,dl3.output,n_in,2048,activation=do_nothing)
 
-        #Layer4: hidden
-        lreg=LogisticRegression(rng,h1.output,1024,params['n_output'])
+        #Layer6: hidden
+        lreg=LogisticRegression(rng,h4.output,300,params['n_output'])
         self.output = lreg.y_pred
 
-        self.params =c1.params+c2.params+c3.params+c4.params+h1.params+lreg.params
+        self.params =c1.params+c2.params+c3.params+h1.params+h2.params+h3.params+h4.params+lreg.params
 
         cost=get_err_fn(self,cost_function,Y)
         L2_reg=0.0001
