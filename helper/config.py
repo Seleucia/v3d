cda95fa65dea08e2b510acd00baabb1aa0cd751e
Seleucia <coskunh1986@gmail.com>
2016-07-07 04:22:37 +0300

    cnn data loading.

--- helper/config.py
+++ helper/config.py
@@ -6,8 +6,8 @@ def get_params():
    global params
    params={}
    params['run_mode']=0 #0,full,1:resume, 2 = combine models
-   params["rn_id"]="lstm" #running id, model
-   params["notes"]="lstm training with  best model,titanx2 machine using" #running id
+   params["rn_id"]="lstm_std008" #running id, model
+   params["notes"]="lstm training with  std=0.008, orijinal was 0.002" #running id
    params["model"]="lstm"#kccnr,dccnr
    params["optimizer"]="Adam" #1=classic kcnnr, 2=patch, 3=conv, 4 =single channcel
    params['mfile']=""
