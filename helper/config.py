e2238f04e98a7e149114dfab8a66b3e1588a7aac
Seleucia <coskunh1986@gmail.com>
2016-07-09 06:46:30 +0300

    cnn data loading.

--- helper/config.py
+++ helper/config.py
@@ -7,7 +7,7 @@ def get_params():
    params={}
    params['run_mode']=0 #0,full,1:resume, 2 = combine models
    params["rn_id"]="lstm_mdn_noreset_bs0_seq50_clipp" #running id, model
-   params["notes"]="lstm training with different mdn" #running id
+   params["notes"]="lstm training with mdn, gradient clipping, bs o" #running id
    params["model"]="lstm_mdn"#kccnr,dccnr
    params["optimizer"]="Adam" #1=classic kcnnr, 2=patch, 3=conv, 4 =single channcel
    params['mfile']=""
