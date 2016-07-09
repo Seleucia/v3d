890fb9f775ce0c1c8be952f0d5fb514cd73cbc4e
Seleucia <coskunh1986@gmail.com>
2016-07-10 01:09:59 +0300

    cnn data loading.

--- helper/config.py
+++ helper/config.py
@@ -17,7 +17,7 @@ def get_params():
    params['mtype']="cnn"
    params['shufle_data']=1
    params['nlayer']= 3 #LSTM
-   params['batch_size']=100
+   params['batch_size']=300
    params['seq_length']= 1
    params['reset_state']= 1#-1=Never, n=every n batch
    params["corruption_level"]=0.5
