ae23b65d131a1e1423435d595a57a4938b143eef
Seleucia <coskunh1986@gmail.com>
2016-07-10 05:07:50 +0300

    cnn data loading.

--- helper/config.py
+++ helper/config.py
@@ -14,7 +14,7 @@ def get_params():
    # params['mfile']= "cnn_1_0.p,lstm_auto_lstm_21_0.0078104_best.p"
    # params['mfile']= "cnn_1_1_24.6707_best2.p,autoencoder_auto_lr_low_138_0.00340542_best.p"
 
-   params['lr']=0.001
+   params['lr']=0.0001
    params['mtype']="seq"
    params['shufle_data']=1
    params['nlayer']= 1 #LSTM
